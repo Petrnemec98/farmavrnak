6985f0e3c2c698ba378ecc7c0394d4d5b6010f0d
Petr Němec <petr.nemec.1998@gmail.com>
2022-03-11 18:44:00 +0100

    Ukázka

---
+++ php/header.php
@@ -0,0 +1,41 @@
+<?php
+
+	//proměnná
+	$page_name = basename($_SERVER['SCRIPT_NAME'], '.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	
+
+		<title>
+			<?php
+			//Title 
+				if ( $page_name == 'index') echo 'Farma Vrňák';
+
+		 	?>	
+		</title>
+
+
+
+		<link rel="icon" href="favicon.ico" type="image/x-icon"  sizes="16x16" >
+		
+		<!-- technické parametry -->
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="robots" content="index, follow">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+		<!-- SEO -->
+		<meta name="title" content="Farma Vrňák">
+		<meta name="description" content="##">
+		<meta name="keywords" content="###">
+	
+		<!--Styly-->	
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+	
