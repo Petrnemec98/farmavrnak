9e949923b6798adbac3d162bacc672ea55d8107c
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2022-05-07 23:13:11 +0200

    David Smrdí

--- maso.php
+++ maso.php
@@ -11,7 +11,7 @@
             <div class="box-h2-title">
               <h1>Maso</h1>
             </div>
-              <h2>O našem mase</h2>
+              <h2>David Smrdí</h2>
             <article>
             <div class="layout-description">
                 <p>
