e8e1eb4959447f21da5635cdc18b64c58560b757
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2022-05-08 12:39:29 +0200

    Cow + shortcut

--- php/overlay.php
+++ php/overlay.php
@@ -1,8 +1,5 @@
       <div id="minutky" onclick="offOverlay(this)">
-        <div id="text">
-          <a href="#1">Maso 1</a>
-          <a href="#2">Maso 2</a>
-        </div>
+        
       </div>
 
       <div id="peceni" onclick="offOverlay(this)">
