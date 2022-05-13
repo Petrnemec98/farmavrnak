f57a01fc76e2ccfdab13643eda7cc8162bc36f53
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2022-05-13 15:27:54 +0200

    overlay-design

--- php/overlay.php
+++ php/overlay.php
@@ -2,7 +2,7 @@
         
 
 
-      <div id="peceni" onclick="offOverlay(this)">
+      <div id="peceni" onclick="offOverlay(this)" >
         <div id="text">
           <a href="#1">Maso 4</a>
           <a href="#2">Maso 5</a>
@@ -17,46 +17,28 @@
       </div>
 
 <div id="minutky">
-
-  <button data-id="minutky" onclick="offOverlay(this)">Zavřít</button>
-
-  <section class="content-1-box">
-  <div class="defaul-max-wight">
-      <div class="half-circule-positon light">
-        <div class="half-circule light">
-        </div>
-      </div>
-    <div class="content">
-      <div class="content-container">
-        <div class="box-h1-prepered">
-              <div class="box-h2-title">
-              <h1>Minutky</h1>
-            </div>
-              <h2>Naše maso</h2>
-            <article>
-            <div class="layout-description">
-                <p>
-                 Děda s babičkou pro základ chovu nakonec zvolili plemeno Limousine, které je odolné, má výtečnou zmasilost zadních částí a je vhodné pro volný chov na pastvinách a krmení senem. Dalším důvodem pro volbu byl fakt, že plemeno má bezproblémové porody, které probíhají bez zásahu člověka – 96 % telat se rodí samovolně. Babičce se také moc líbila hnědá barva těchto kraviček a jejich vznešené jméno. Hlavně nechtěla kravky s bílou hlavou, na které byla zvyklá u tehdy běžného masného simentálu.
-                </p>
-            </div>
-          </article>
-        </div>
-      </div>
-            <div class="bio-icon-bar">
-        <a href="#2" data-id="minutky" onclick="offOverlay(this)"><div class="bio-icon green ">
-        </div></a>
-        <a href="#"><div class="bio-icon meat  ">
-        </div></a>
-        <a href="#"><div class="bio-icon cow-icon ">
-        </div></a>
-      </div>
-    </div >
-    <div class="slideshow-container">
-      <div class="our-story-pics right">
-
-      </div>
+ <div class="overlay-background">
+  <section class="overlay-box">
+    <div class="overlay-box-close">
+      <a class="overlay-close" data-id="minutky" onclick="offOverlay(this)">
+      </a>
     </div>
-  </div>
-</section>
+    <div class="box-h1-prepered">
+        <div class="box-h2-title">
+          <h1>Na minutkovou úpravu</h1>
+        </div>
+          <h2>Hovězí maso</h2>
 
-      </div>
+    <article>
+      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+        <span>Hruška</span>
+      </a>
+    </article>
+  </section>
+ 
+  </div>
+
+    </div>
+
+
+            
