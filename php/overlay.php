58d8d27843f390a1316d0e5738c77b05961f309a
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2022-05-13 17:56:56 +0200

    minutky/peceni/duseni

--- php/overlay.php
+++ php/overlay.php
@@ -1,69 +1,200 @@
 <div id="minutky">
  <div class="overlay-background">
-  <section class="overlay-box">
-    <div class="overlay-box-close">
-      <a class="overlay-close" data-id="minutky" onclick="offOverlay(this)">
-      </a>
-    </div>
-    <div class="box-h1-prepered">
+    <section class="overlay-box">
+      <div class="overlay-box-close">
+        <a class="overlay-close" data-id="minutky" onclick="offOverlay(this)">
+        </a>
+      </div>
+      <div class="box-h1-prepered">
         <div class="box-h2-title">
           <h1>Na minutkovou úpravu</h1>
         </div>
           <h2>Hovězí maso</h2>
-
-
-    </div>
-
-
-
-    <div class="overlay-box-content">
-    <article>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Hruška</span>
-      </a>
-       <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Tabulová špička</span>
-      </a>
-       <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Holubička</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Květová špička</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Loupaná plec</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Pravá svíčková</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Nízký roštěnec</span>
-      </a>
-    </article>
-    <article>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Vysoký roštěnec</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Vnitřní oponka</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Husička</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Pavouček</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Veverka</span>
-      </a>
-      <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
-        <span>Oponka</span>
-      </a>
-    </article>
-  </div>
-  </section>
+      </div>
+      <div class="overlay-box-content">
+        <article>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Hruška</span>
+          </a>
+           <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Tabulová špička</span>
+          </a>
+           <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Holubička</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Květová špička</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Loupaná plec</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Pravá svíčková</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Nízký roštěnec</span>
+          </a>
+        </article>
+        <article>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Vysoký roštěnec</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Vnitřní oponka</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Husička</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Pavouček</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Veverka</span>
+          </a>
+          <a href="#hruska" data-id="minutky" onclick="offOverlay(this)">
+            <span>Oponka</span>
+          </a>
+        </article>
+      </div>
+    </section>
   </div>
 </div>
 
+<div id="peceni">
+ <div class="overlay-background">
+    <section class="overlay-box">
+      <div class="overlay-box-close">
+        <a class="overlay-close" data-id="peceni" onclick="offOverlay(this)">
+        </a>
+      </div>
+      <div class="box-h1-prepered">
+        <div class="box-h2-title">
+          <h1>Na pečení</h1>
+        </div>
+          <h2>Hovězí maso</h2>
+      </div>
+      <div class="overlay-box-content">
+        <article>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Vrchní šál</span>
+          </a>
+           <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Spodní šál</span>
+          </a>
+           <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Předkýtí (velký ořech)</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Váleček</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Velká plec</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Loupaná plec</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Kulatá plec</span>
+          </a>
+        </article>
+        <article>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Podplečí</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Krk</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Karabáček</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Nízký roštěnec</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Bok s kostí</span>
+          </a>
+          <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Žebra</span>
+          </a>
+           <a href="#hruska" data-id="peceni" onclick="offOverlay(this)">
+            <span>Hrudí</span>
+          </a>
+        </article>
+      </div>
+    </section>
+  </div>
+</div>
+
+<div id="duseni">
+ <div class="overlay-background">
+    <section class="overlay-box">
+      <div class="overlay-box-close">
+        <a class="overlay-close" data-id="duseni" onclick="offOverlay(this)">
+        </a>
+      </div>
+      <div class="box-h1-prepered">
+        <div class="box-h2-title">
+          <h1>Na dušení</h1>
+        </div>
+          <h2>Hovězí maso</h2>
+      </div>
+      <div class="overlay-box-content">
+        <article>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Vrchní šál</span>
+          </a>
+           <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Spodní šál</span>
+          </a>
+           <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Předkýtí (velký ořech)</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Nízký roštěnec</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Váleček</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Velká plec</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Loupaná plec</span>
+          </a>
+     
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Kulatá plec</span>
+          </a>
+        </article>
+        <article>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Podplečí</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Krk</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Zadní kližka</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Karabáček</span>
+          </a>
+          <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Přední kližka</span>
+          </a>
+           <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Plátek Lopatkový</span>
+          </a>
+           <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Líčka</span>
+          </a>
+           <a href="#duseni" data-id="peceni" onclick="offOverlay(this)">
+            <span>Hrudí</span>
+          </a>
+        </article>
+      </div>
+    </section>
+  </div>
+</div>
 
             
