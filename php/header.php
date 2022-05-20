4cd735c0f71cac68e182078257a5971d36a42061
Petrnemec98 <103196707+Petrnemec98@users.noreply.github.com>
2022-05-20 17:53:27 +0200

    cache

--- php/header.php
+++ php/header.php
@@ -30,6 +30,14 @@
 <html lang="cs">
 <head>
 
+<!-- cache-->
+<IfModule mod_expires.c>
+    ExpiresActive On
+    ExpiresDefault A600
+    ExpiresByType assets A2678400
+    ExpiresByType php A2678400
+</IfModule>
+
 <!-- Google Tag Manager -->
 <!--SEONO
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
